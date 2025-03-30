4372b6b27b02c2cc847325a90ad71fd20e3e7ed1
Shruti Debnath <debnathshruti477@gmail.com>
2025-03-29 17:14:12 -0700

    TL5 added parts

--- legend-of-warriors/Assets/src/TL1/SceneManagerScript.cs
+++ legend-of-warriors/Assets/src/TL1/SceneManagerScript.cs
@@ -3,15 +3,38 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagerScript : MonoBehaviour
 {
-    // Load Shop scene additively
+    public delegate void ApplyChangesDelegate();
+    public static event ApplyChangesDelegate OnApplyAll;
+
+    public delegate void DiscardChangesDelegate();
+    public static event DiscardChangesDelegate OnDiscardAll;
+
+    // Load the Shop scene
     public void OpenShop()
     {
         SceneManager.LoadScene("Shop", LoadSceneMode.Additive);
     }
-    // Return to the Main Menu from the Shop scene
+
+    // Return to the Main Menu
     public void ReturnToMainMenu()
     {
         SceneManager.LoadScene("game-lobby", LoadSceneMode.Additive);
         SceneManager.UnloadSceneAsync("Shop");
+    }
+
+    // Called by "Apply All" button
+    public void ApplyAllChanges()
+    {
+        OnApplyAll?.Invoke(); // Notifies CustomizationPanelManager
+        Debug.Log("All selections applied.");
+        ReturnToMainMenu();
+    }
+
+    // Called by "Discard All" button
+    public void DiscardAllChanges()
+    {
+        OnDiscardAll?.Invoke(); // Notifies CustomizationPanelManager
+        Debug.Log("All selections discarded.");
+        ReturnToMainMenu();
     }
 }
